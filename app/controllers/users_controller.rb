cda403dc1e552c063c3d5a188b9b148cbfe5fd8b
akie-koshimizu <akie0122@icloud.com>
2020-11-04 21:16:27 +0900

    テストコード実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = user.new(user_params)
+    if @user.valid?
+      @user.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,20 @@ class User < ApplicationRecord
   has_many :buyers
        
   validates :nickname, presence: true
+  validates :birthday, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_name_kana, presence: true
   validates :last_name_kana, presence: true
+  
+  
+  with_options presence: true do
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+  end
   validates :birthday, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 end

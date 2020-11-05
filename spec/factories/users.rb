fa072699b33a097d1c82410bce349acc0c6cf349
akie-koshimizu <akie0122@icloud.com>
2020-11-05 12:21:59 +0900

    ユーザー管理機能修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {"111aaa"}
     password_confirmation {password}
     first_name            {"花子"}
     last_name             {"田中"}
